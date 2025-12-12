fd6ed7e89342a0d84d17c6dac8e26cc1f8d6fa38
Yogesh <yoogesh1235@gmail.com>
2025-12-12 12:21:17 +0530

    changing the spaces

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,21 +3,20 @@ import { assets } from "../assets/assets";
 import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 import { Link, useNavigate } from "react-router-dom"; // ✅ Import Link
 import { AppContext } from "../context/AppContext";
-///enfnvknfv
+///  enfnvknfv
 const Navbar = () => {
   const { openSignIn } = useClerk();
   const { user } = useUser();
 
   const navigate = useNavigate()
 
-  const {setShowRecruiterLogin} = useContext(AppContext)
+  const { setShowRecruiterLogin } = useContext(AppContext)
 
   return (
     <div className="shadow py-4">
       <div className="container px-4 2xl:px-20 mx-auto flex justify-between items-center">
         {/* Logo */}
-        <img onClick={() => navigate('/')} className='cursor-pointer h-10' src={assets.logo} alt="Logo"  />
-
+        <img onClick={() => navigate('/')} className='cursor-pointer h-10' src={assets.logo} alt="Logo" />
         {user ? (
           <div className="flex items-center gap-6">
             {/* Applied Jobs Link */}
@@ -35,7 +34,7 @@ const Navbar = () => {
           </div>
         ) : (
           <div className="flex gap-4 max-sm:text-xs">
-            <button onClick ={e => setShowRecruiterLogin(true)}className="text-gray-600">Recruiter Login</button>
+            <button onClick={e => setShowRecruiterLogin(true)} className="text-gray-600">Recruiter Login</button>
             <button
               onClick={() => openSignIn()}
               className="bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full"
